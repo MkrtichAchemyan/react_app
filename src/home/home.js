78ead32b362632364e2a5a2ee73175774127035a
MkrichAchemyan <mkrtichachemyan@gmail.com>
2019-03-17 12:50:21 +0400

    Create

---
+++ src/home/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <h1 className="hello">hello world</h1>
+        );
+    }
+}
+
+export default Home;
